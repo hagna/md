e787bf7575440d8e8ebdc97a063d01d149fdc11e
Nate <us@sickypup.(none)>
2012-09-02 15:31:21 -0600

    handle comments too

--- test/html2nml.py
+++ test/html2nml.py
@@ -10,18 +10,44 @@ TAG = 0
 ATTR = 1
 CONTENT = 3
 ENDTAG = 4
+COMMENT = 5
+comment = False
 while line:
 	while line:
+		#print "work on line %r[%d] at %r" % (line, len(line), line[i:])
 		s = i
-		while (i < len(line) and line[i] != '<'):
-			i += 1
-		buf += line[s:i]
-		if i == len(line):
-			line = sys.stdin.readline()
-			i = 0
-			continue
+		if comment:
+			#print "searching for close comment starting at %d %r" % (i, line[i:])
+			while (i < len(line) and not(line[i-2] == '-' and
+						line[i-1] == '-' and
+						line[i] == '>')):
+				i += 1
+				#print i
+			if i >= len(line):
+				buf += line[s:i]
+				line = sys.stdin.readline()
+				i = 0
+				continue
+			else:
+				buf += line[s:i-2]
+				#print "COMMENT buffer found", buf
+				stack.append((COMMENT, buf))
+				buf = ''
+				#print "Found close comment at %d" % i
+				comment = False
+				break
 		else:
-			break
+			while (i < len(line) and line[i] != '<'):
+				i += 1
+				#print i
+			buf += line[s:i]
+			if i >= len(line):
+				line = sys.stdin.readline()
+				i = 0
+				continue
+			else:
+				break
+			#print "found tag token at", i
 	# line[i] is the begin tag token
 	j = i
 
@@ -30,8 +56,17 @@ while line:
 	while (j < len(line) and line[j] != '>' and line[j] != ' '):
 		j += 1
 	tagname = line[i+1:j]
+	#print "found tag ", tagname	
+	if tagname.startswith('!--'):
+		#print "Found comment at %d" % i
+		comment = True
+		if buf:
+			stack.append((CONTENT, buf))
+			buf = ''
+		i = j+1
+		continue
 	if tagname == '':
-		print "no tag"
+		#print "%r contains no tag at index %d:%d" % (line, i+1, j)
 		continue
 	if j < len(line) and line[j] == ' ':
 		k = j
@@ -53,8 +88,12 @@ while line:
 last = None
 for t, val in stack:
 	if t == TAG:
-		res += '<%s ' % val
-		last = TAG
+		if last == TAG:
+			res += '| <%s ' % val
+			last = TAG
+		else:
+			res += '<%s ' % val
+			last = TAG
 	if t == ATTR:
 		val = val.strip()
 		attrs = val.split(' ')
@@ -69,6 +108,8 @@ for t, val in stack:
 		else:
 			res += '| %s' % val
 		last = CONTENT
+	if t == COMMENT:
+		res += "<!--| %s>" % val
 	if t == ENDTAG:
 		res += '>'
 		last = ENDTAG
