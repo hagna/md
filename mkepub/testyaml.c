49f76c24774dfd36238685cc16de6ab199c9b1da
Nate <us@sickypup.(none)>
2012-09-15 19:08:36 -0600

    testyaml prints out interesting stuff

--- mkepub/testyaml.c
+++ mkepub/testyaml.c
@@ -3,7 +3,7 @@
 
 int main(void) 
 {
-	FILE *fh = fopen("config/public.yaml", "r");
+	FILE *fh = fopen("test.yaml", "r");
 	yaml_parser_t parser;
 	yaml_token_t token;
 
@@ -14,7 +14,42 @@ int main(void)
 	
 	yaml_parser_set_input_file(&parser, fh);
 
+
+	do {
+		yaml_parser_scan(&parser, &token);
+		switch (token.type)
+		{
+			case YAML_STREAM_START_TOKEN: puts("STREAM START");
+				break;
+			case YAML_STREAM_END_TOKEN: puts("STREAM END");
+				break;
+			case YAML_KEY_TOKEN: printf("(Key token)    "); 
+				break;
+			case YAML_VALUE_TOKEN: printf("(Value token)    ");
+				break;
+			case YAML_BLOCK_SEQUENCE_START_TOKEN:
+				puts("<b|Start Block (Sequence>"); break;
+			case YAML_BLOCK_ENTRY_TOKEN: 
+				puts("<b|Start Block (Entry)>"); break;
+			case YAML_BLOCK_END_TOKEN: 
+				puts("<b|End block>");
+			case YAML_BLOCK_MAPPING_START_TOKEN:
+				puts("[Block mapping]");
+				break;
+			case YAML_SCALAR_TOKEN:
+				printf("scalar %s\n", token.data.scalar.value);
+				break;
+			default:
+				printf("Got token of typed %d\n", token.type);
+		}
+		if (token.type != YAML_STREAM_END_TOKEN)
+			yaml_token_delete(&token);
+	} while (token.type != YAML_STREAM_END_TOKEN);
+	yaml_token_delete(&token);
+
+	
 	yaml_parser_delete(&parser);
+
 	fclose(fh);
 	return 0;
 }
